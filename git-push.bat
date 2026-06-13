f37bf992f4722be082c14fa62b250ac024a3c56b
lovesweater <lds2002@126.com>
2026-06-12 19:45:32 -0700

    V30 update

---
+++ git-push.bat
@@ -0,0 +1,21 @@
+@echo off
+cd /d "%~dp0"
+
+echo Setting remote...
+git remote set-url origin https://github.com/lovesewater/linfairwool.git
+
+echo Adding files...
+git add .
+
+echo Committing...
+git commit -m "V30 update"
+
+echo Pushing to GitHub...
+git push -u origin main
+
+if %errorlevel% equ 0 (
+    echo SUCCESS! Cloudflare will auto-deploy.
+) else (
+    echo FAILED. Check your network.
+)
+pause
